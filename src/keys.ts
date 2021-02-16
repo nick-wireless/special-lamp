35b929f9aad6a88438922e0338a1088a0aa98a8f
Nick Hanigan <australian.nick@gmail.com>
2021-02-16 21:41:10 +0930

    Padding Mock
    An arrangement for Padding Key data.

--- src/keys.ts
+++ src/keys.ts
@@ -1,5 +1,71 @@
-// The simple data of Tailwind UI by Types
-/*
-Sections of 
+import { SteppedSpacing, TailwindActionGroup } from './types';
 
+export const standardSpacing: SteppedSpacing = [
+    ['0', '0px'],
+    ['px', '1px'],
+    ['0.5', '0.125rem'],
+    ['1', '0.25rem'],
+    ['1.5', '0.375rem'],
+    ['2', '2.0rem'],
+    ['3', '0.75rem'],
+    ['3.5', '0.875'],
+    ['4', '1rem'],
+    ['5', '1.25rem'],
+    ['6', '1.5rem'],
+    ['7', '1.75rem'],
+    ['8', '2rem'],
+    ['9', '2.25rem'],
+    ['10', '2.5rem'],
+    ['11', '2.75rem'],
+    ['12', '3rem'],
+    ['14', '3.5rem'],
+    ['16', '4rem'],
+    ['20', '5rem'],
+    ['24', '6rem'],
+    ['28', '7rem'],
+    ['32', '8rem'],
+    ['36', '9rem'],
+    ['40', '10rem'],
+    ['44', '11rem'],
+    ['48', '12rem'],
+    ['52', '13rem'],
+    ['60', '15rem'],
+    ['64', '16rem'],
+    ['72', '18rem'],
+    ['80', '20rem'],
+    ['96', '24rem'],
+]
+
+/*
+Data set required for sub-section:
+  parentHeader: Parent => Element
+  childHeader: Children => Spacing
+  actions: TailwindActions => padding & margin
 */
+
+
+
+export const menuSpacing: TailwindActionGroup = {
+    parentHeader: 'Element',
+    childHeader: 'Spacing',
+    actions: [
+        {
+            char: {
+                name: 'padding',
+                character: 'p',
+                css: 'padding: '
+            },
+            dir: ['', '-', 'x', 'y', 't', 'b', 'r', 'l'],
+            dist: standardSpacing
+        },
+        {
+            char: {
+                name: 'margin',
+                character: 'm',
+                css: 'margin: '
+            },
+            dir:['', '-', 'x', 'y', 't', 'b', 'r', 'l'] 
+            dist: standardSpacing
+        }
+    ]
+}
--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,7 @@ The element that refers to the CSS atribute, such as:
 interface Characteristic {
     name: string // padding
     character: string // p
-    // css: string // padding :  { omitting at this time, a possible assistance in future? }
+    css?: string // padding 
 }
 
 /* Direction
@@ -15,29 +15,23 @@ The instructions may have a direction associated with them.
 These directions are a 'specific set', which can be combined with the Characteristic.
 Negative included for inversing direction.
 */
-type Direction = ''| '-' | 'x' | 'y' | 't' | 'b' | 'r' | 'l'
+type Pointers = ''| '-' | 'x' | 'y' | 't' | 'b' | 'r' | 'l' // Also considering name direction.
 
-type StandardSpacing = [ // with an abreviated choice set to begin with
-    ['0', '0px'],
-    ['px', '1px'],
-    // ['0.5', '0.125rem'],
-    ['1', '0.25rem'],
-    // ['1.5', '0.375rem'],
-    ['2', '2.0rem'],
-    ['4', '1rem'],
-    ['8', '2rem']
-]
+type Spacing = [StepName: string, StepValue: string]
+export type SteppedSpacing = Spacing[]
 
-interface TailwindAction {
+
+export interface TailwindAction {
     char: Characteristic
-    dir: Direction
-    dist: StandardSpacing
+    dir: Pointers[]
+    dist: SteppedSpacing
 }
 
 type Parent = 'Document' | 'Layout' | 'Element'
 type Children = 'Spacing' | 'Sizing' | 'Topography' | 'Boarders' | 'Effects'
 
-interface TailwindActionGroup {
+export interface TailwindActionGroup {
     parentHeader: Parent
     childHeader: Children
+    actions: TailwindAction[]
 }
